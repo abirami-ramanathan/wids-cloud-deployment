6205c8bda4e5e9eb79fa3c11b9938250c036d7c5
abirami-ramanathan <abiramiramanathan14@gmail.com>
2026-02-26 10:44:51 +0530

    Updated app to live simulation mode

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import Flask, request
 import subprocess
+import datetime
+import random
 
 app = Flask(__name__)
 
+LOG_FILE = "sample_logs.txt"
+
 @app.route("/")
 def home():
-    return "Web Intrusion Detection System Running 🚀"
+    return "Web Intrusion Detection System Running"
+
+@app.route("/simulate")
+def simulate():
+    # Simulate incoming web log
+    ip = request.remote_addr
+    timestamp = datetime.datetime.utcnow().strftime("%d/%b/%Y:%H:%M:%S +0000")
+    endpoint = random.choice(["/", "/admin", "/.git/config", "/search?q=test"])
+    status = random.choice([200, 403, 500])
+    size = random.randint(200, 4000)
+    ua = request.headers.get("User-Agent")
+
+    log_line = f'{ip} - - [{timestamp}] "GET {endpoint} HTTP/1.1" {status} {size} "-" "{ua}"\n'
+
+    with open(LOG_FILE, "a") as f:
+        f.write(log_line)
+
+    return "Live log generated"
 
 @app.route("/detect")
 def detect():
